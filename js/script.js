2e7847696277b646f4f71fcbabd5436c40ca67b3
Jose Froes <josepaulo.froes@gmail.com>
2024-03-19 19:04:21 -0300

    minor changes on slider

--- js/script.js
+++ js/script.js
@@ -4,15 +4,13 @@ function darkMode() {
   function handleClick() {
     document.body.classList.toggle("adaptative");
     if (document.body.classList.contains("adaptative")) {
-      botao.src =
-        "http://jose-froes-portfolio.local/wp-content/uploads/2024/03/sun-svgrepo-com.png";
+      botao.src = "./wp-content/uploads/2024/03/sun-svgrepo-com.png";
       botao.setAttribute(
         "src",
-        "http://jose-froes-portfolio.local/wp-content/uploads/2024/03/sun-svgrepo-com.png"
+        "./wp-content/uploads/2024/03/sun-svgrepo-com.png"
       );
     } else {
-      botao.src =
-        "http://jose-froes-portfolio.local/wp-content/uploads/2024/03/moon-svgrepo-com.png";
+      botao.src = "./wp-content/uploads/2024/03/moon-svgrepo-com.png";
     }
   }
 
@@ -25,6 +23,17 @@ function initSwiper() {
     direction: "horizontal",
     slidesPerView: 3,
     loop: true,
+    grabCursor: true,
+    spaceBetween: 0,
+    // freeMode: true,
+
+    // autoplay: {
+    //   delay: 1,
+    //   disableOnInteraction: true,
+    // },
+    // freeMode: true,
+    // speed: 5000,
+    // freeModeMomentum: false,
 
     // If we need pagination
     pagination: {
@@ -32,16 +41,19 @@ function initSwiper() {
       clickable: true,
       dynamicBullets: true,
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
     },
   });
 }
